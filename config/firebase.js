ab2ed5c9c721edd36e1d784f9f7f14deb76f8e87
elora <108570316+eloraistired@users.noreply.github.com>
2023-11-26 13:15:12 +0000

    set custom claims to firebase user

--- config/firebase.js
+++ config/firebase.js
@@ -7,14 +7,17 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
-exports.setCustomClaims = async (claim, email, value) => {
+exports.setCustomClaims = async (claim, email, value, nooverride) => {
     const user = await admin.auth().getUserByEmail(email);
     console.log(user.customClaims[claim], value, claim);
     if (user.customClaims[claim] === value) {
         return;
     }
-    await admin.auth().setCustomUserClaims(user.uid, {});
+    if (!nooverride) {
+        const field = claim === "admin" ? "moderator" : "admin";
+        await admin.auth().setCustomUserClaims(user.uid, { [field]: null });
+    }
     return admin.auth().setCustomUserClaims(user.uid, {
-        admin: null,
+        [claim]: value,
     });
 };
