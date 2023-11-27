c65fc7fb2bc52a0a018e70924eb0e5d1e4d8ffe8
elora <108570316+eloraistired@users.noreply.github.com>
2023-11-27 08:39:32 +0000

    fixed the custom claims

--- config/firebase.js
+++ config/firebase.js
@@ -1,22 +1,21 @@
 const admin = require("firebase-admin");
 const { firebaseConfig } = require("./vars");
 
-const serviceAccount = require(firebaseConfig);
+const serviceAccount = firebaseConfig
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
-exports.setCustomClaims = async (claim, email, value, nooverride) => {
+exports.setCustomClaims = async (claim, email, value) => {
     const user = await admin.auth().getUserByEmail(email);
     if (user.customClaims && user.customClaims[claim] === value) {
         return;
     }
-    if (!nooverride) {
-        const field = claim === "admin" ? "moderator" : "admin";
-        await admin.auth().setCustomUserClaims(user.uid, { [field]: null });
-    }
+
     return admin.auth().setCustomUserClaims(user.uid, {
-        [claim]: value,
+        subscribed: user.customClaims?.subscribed || claim === 'subscribed' ? value : null,
+        admin: user.customClaims?.admin && claim !== 'moderator' || claim === 'admin' ? value : null,
+        moderator: user.customClaims?.moderator && claim !== 'admin' || claim === 'moderator' ? value : null,
     });
 };
--- config/vars.js
+++ config/vars.js
@@ -6,6 +6,8 @@ require('dotenv-safe').config({
   sample: path.join(__dirname, '../.env.example'),
 });
 
+
+
 module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
@@ -16,7 +18,7 @@ module.exports = {
   mongo: {
     uri: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TESTS : process.env.MONGO_URI,
   },
-  firebaseConfig: process.env.FIREBASE_CONFIG,
+  firebaseConfig: require('../admin.json'),
   stripeSecretKey: process.env.STRIPE_SECRET_APIKEY,
   logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
   favicon: '<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none"> <style> path { fill: black; } @media (prefers-color-scheme: dark) { path { fill: white; } } </style> <path d="M20.9785 10.7656C20.9785 16.2885 16.5014 20.7656 10.9785 20.7656C5.45567 20.7656 0.978516 16.2885 0.978516 10.7656C0.978516 5.24278 5.45567 0.765625 10.9785 0.765625C16.5014 0.765625 20.9785 5.24278 20.9785 10.7656Z" fill="#0016EC" /> </svg>'
